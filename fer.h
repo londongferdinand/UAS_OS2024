d276e724785db4e27266c53ecaa5b0a68611dd77
londongferdinand <ferdinandlondong@gmail.com>
2024-12-09 16:53:33 +0900

    first commit

---
+++ fer.h
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+void dolar();
+void rupiah();
+
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "fer.h"
+int main ()
+{
+    printf("\t\t Library sistem operasi\n");
+    printf("\n ============================== \n");
+    printf("Welcome to converter money");
+    printf("\n ============================== \n");
+    printf("\n nilai dolar ke rupiah \n");
+    rupiah();
+    
+    printf("\n nilai rupiah ke dolar \n");
+    dolar();
+}
